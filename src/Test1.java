934df4f5705668d11df793edcb366fc6432167fb
punarvi <karanampunarvi@gmail.com>
2024-03-24 16:08:25 +0800

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void Newtest ()
+	{
+		System.out.println("New testcase");
+		
+	}
+	
+	@Test
+	public void Newtest1()
+	{
+		System.out.println("New testcase1");
+	}
+	
+	@Test
+	public void Newtest3()
+	{
+		System.out.println("New testcase2");	
+	}
+
+}
